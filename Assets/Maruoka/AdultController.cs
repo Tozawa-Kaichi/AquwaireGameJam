3ed47eeeeccc74881669e95031438706172d5103
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-08-18 14:31:43 +0900

    AdultController.csファイルを作成

---
+++ Assets/Maruoka/AdultController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdultController : MonoBehaviour
+{
+    public bool _isSurprised = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
