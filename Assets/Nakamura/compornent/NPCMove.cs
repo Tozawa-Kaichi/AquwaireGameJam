136bb4f3704f5410035be928a739e14a9a52e5b9
HarumiMaruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2022-08-19 10:56:18 +0900

    NPCMoveの _moveSpeed を public に変更

--- Assets/Nakamura/compornent/NPCMove.cs
+++ Assets/Nakamura/compornent/NPCMove.cs
@@ -7,7 +7,7 @@ public  class NPCMove : MonoBehaviour
     /// <summary>NPCが移動できるか /// </summary>
     [Header("移動可能か否か"), SerializeField] public bool _isMove = true;
     /// <summary>NPCの移動速度/// </summary>
-    [Header("NPCの移動速度"), SerializeField] float _moveSpeed = 5;
+    [Header("NPCの移動速度"), SerializeField] public float _moveSpeed = 5;
     /// <summary>objectのデータを取ってくる（主にposition）/// </summary>
     [Header("追尾先のオブジェクト"),SerializeField] Transform _followObject;
     Rigidbody2D _rb2d;
